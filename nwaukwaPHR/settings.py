ea8669ff15041107f146753c4d4358cae94c40e3
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-07-13 15:58:02 -0500

    add user app to installed apps

--- nwaukwaPHR/settings.py
+++ nwaukwaPHR/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'health_records.apps.HealthRecordsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
