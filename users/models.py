53e0b7d8a130b3114bb243ba52cf5dce95df2e5f
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-07-25 22:45:23 -0500

    create user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class NewUserData(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email_address = models.EmailField()
