8d6dbec196e0a08db037f52297f144af40ceaf81
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-07-27 07:52:21 -0500

    update model

--- users/views.py
+++ users/views.py
@@ -11,6 +11,7 @@ def register(request):
         form = NewUserForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data
+            form.save()
     else:
         form = NewUserForm()
     return render(request, 'users/register.html', {'form': form})
