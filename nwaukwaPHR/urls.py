26d9ba4daee92a5744d41e36c2cc79ce68447f5f
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-06 08:19:37 -0500

    add register url

--- nwaukwaPHR/urls.py
+++ nwaukwaPHR/urls.py
@@ -19,6 +19,6 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
+    path('register/', include('users.urls')),
     path('', include('health_records.urls')),
 ]
