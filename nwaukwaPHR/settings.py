600fd997a8ac65cacd5c6d50154104965793bd81
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-06 08:32:18 -0500

    add members app to settings

--- nwaukwaPHR/settings.py
+++ nwaukwaPHR/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'members.apps.MembersConfig',
     'health_records.apps.HealthRecordsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
