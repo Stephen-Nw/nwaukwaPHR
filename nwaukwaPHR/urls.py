cc956fa52b91dc22d4febbf024ec1901e187708d
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-06 10:03:58 -0500

    add members url

--- nwaukwaPHR/urls.py
+++ nwaukwaPHR/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('health_records.urls')),
+    path('members/', include('members.urls')),
 ]
