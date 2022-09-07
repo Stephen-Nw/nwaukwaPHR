efe871354760af46e4788aed58e8ad41a632fd80
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-07 08:46:10 -0500

    create user register view

--- members/views.py
+++ members/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def user_login(request):
     return render(request, 'authenticate/login.html')
+
+
+def user_register(request):
+    return render(request, 'authenticate/register_user.html')
