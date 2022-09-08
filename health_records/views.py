49517669e39029a13d0aaa418bbe9817f11d9375
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-08 14:42:19 -0500

    create user profile view

--- health_records/views.py
+++ health_records/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, "health_records/index.html")
+
+
+def user_profile(request):
+    return render(request, 'user_profile.html')
