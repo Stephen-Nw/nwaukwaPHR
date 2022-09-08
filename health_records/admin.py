48f1ed33fbc7d6574fb5d305e95eed938c7b0a4e
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-07 22:07:34 -0500

    cutomize admin user profile display

--- health_records/admin.py
+++ health_records/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import UserProfile
 
 # Register your models here.
-admin.site.register(UserProfile)
+# admin.site.register(UserProfile)
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name',
+                    'date_of_birth', 'email', 'phone')
