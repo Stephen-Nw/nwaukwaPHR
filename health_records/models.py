7c626c20df056f92e7bd40267cffcc994e45e320
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-07 21:32:17 -0500

    fix errors

--- health_records/models.py
+++ health_records/models.py
@@ -34,14 +34,14 @@ class UserProfile(models.Model):
 
     ]
 
-    prefix = models.CharField('Prefix', max_length=2,
+    prefix = models.CharField('Prefix', max_length=4,
                               blank=True, choices=PREFIX_CHOICES, default=NONE)
     first_name = models.CharField('First Name', max_length=50, blank=False)
     last_name = models.CharField('Last Name', max_length=50, blank=False)
     date_of_birth = models.DateField('Date of Birth', blank=False)
-    height_ft = models.IntegerField('Height(ft)', max_length=2, blank=False)
-    height_in = models.IntegerField('Height(in)', max_length=2, blank=True)
-    weight_lbs = models.IntegerField('Weight(lbs)', max_length=3, blank=False)
+    height_ft = models.IntegerField('Height(ft)', blank=False)
+    height_in = models.IntegerField('Height(in)', blank=True)
+    weight_lbs = models.IntegerField('Weight(lbs)', blank=False)
     blood_type = models.CharField('Blood Type', max_length=10, blank=False)
     address = models.CharField('Address', max_length=150, blank=True)
     email = models.EmailField('Email', max_length=150, blank=True)
@@ -50,10 +50,10 @@ class UserProfile(models.Model):
     pcp_address = models.CharField(
         'Provider Address', max_length=120, blank=True)
     pcp_number = models.IntegerField(
-        'Provider Number', max_length=20, blank=True)
+        'Provider Number', blank=True)
     emergency_contact = models.CharField(
         'Emergency Contact', max_length=50, blank=True)
     emergency_phone = models.CharField(
         'Emergency Phone Number', max_length=20, blank=True)
     relationship = models.CharField(
-        'Relationship', max_length=2, choices=RELATIONSHIP_CHOICES, default=SELF)
+        'Relationship', max_length=3, choices=RELATIONSHIP_CHOICES, default=SELF)
