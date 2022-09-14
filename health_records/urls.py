7dc0a98f15bf545c17f6da1bbbb6573e8042496a
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-14 08:16:40 -0500

    create user meds url

--- health_records/urls.py
+++ health_records/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.homepage, name='homepage-info'),
     path('user_profile', views.user_profile, name='user_profile'),
     path('user_allergy', views.user_allergy, name='user_allergy'),
+    path('user_meds', views.user_meds, name='user_meds'),
 ]
