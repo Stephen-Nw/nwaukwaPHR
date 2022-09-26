e2b14531e48878b1205019daa374d9b937354683
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-26 18:00:28 -0500

    change field to charfield

--- health_records/models.py
+++ health_records/models.py
@@ -17,8 +17,8 @@ class ImmunizationProfile(models.Model):
     vaccine_date = models.DateField('Date Last Received', blank=False)
     vaccine_reaction = models.CharField(
         'Adverse Reactions', blank=False, max_length=3, choices=REACTION_CHOICES)
-    vaccine_rxn_if_positive = models.TextField(
-        'Describe reaction (Leave blank if none)', blank=True, null=True)
+    vaccine_rxn_if_positive = models.CharField(
+        'Describe reaction (Leave blank if none)', max_length=50, blank=True, null=True)
     vaccine_next_due = models.DateField('Next Due Date', blank=True)
     vaccine_notes = models.TextField('Additional Notes', blank=True, null=True)
 
