02417d35c9889137514cd896c16aa7d94d8b11c7
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-09-30 11:12:21 -0500

    update str

--- health_records/models.py
+++ health_records/models.py
@@ -100,7 +100,7 @@ class MedicalHistoryProfile(models.Model):
         'UserProfile', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.hx_date
+        return self.hx_diagnosis
 
 
 # Model based on user's appointments
