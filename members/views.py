cdd06c3075cdc21a64969b72cc8cfa064b2838d1
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-12-09 07:44:39 -0600

    update folder name

--- members/views.py
+++ members/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def user_login(request):
-    return render(request, 'authenticate/login.html')
+    return render(request, 'accounts/login.html')
 
 
 def user_register(request):
-    return render(request, 'authenticate/register_user.html')
+    return render(request, 'accounts/register_user.html')
