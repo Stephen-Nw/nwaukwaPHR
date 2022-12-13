80baff17089a984681b102b63c0edfa946ae5d01
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-12-12 21:38:43 -0600

    refactor page

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
+from members.views import (
+    user_login,
+    user_register,
+)
+
+app_name = 'members'
 
 urlpatterns = [
-    path('user_login', views.user_login, name='login'),
-    path('user_register', views.user_register, name='register'),
+    path('user_login', user_login, name='login'),
+    path('user_register', user_register, name='register'),
 
 ]
