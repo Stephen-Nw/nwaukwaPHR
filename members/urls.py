76204ecda7bbad12d13b7cb6d47412838a1a098c
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-12-15 10:47:04 -0600

    update

--- members/urls.py
+++ members/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from members.views import (
-    user_login,
-    user_register,
-)
+# from members.views import (
+#     user_login,
+#     user_register,
+# )
+from . import views
 
 app_name = 'members'
 
 urlpatterns = [
-    path('user_login', user_login, name='login'),
-    path('user_register', user_register, name='register'),
+    path('user_login', views.user_login, name='login'),
+    path('user_register', views.user_register, name='register'),
 
 ]
