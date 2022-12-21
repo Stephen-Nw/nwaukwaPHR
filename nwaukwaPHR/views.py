261df3fd9fa86bf6b4d77b33f99ef7b22ca2b750
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-12-21 09:07:04 -0600

    add login error notification

--- nwaukwaPHR/views.py
+++ nwaukwaPHR/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+
+
+# def home_login_view(request):
+#     """Renders hompage and user login form"""
+#     if request.method == "POST":
+#         form = AuthenticationForm(request, data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('user_profile')
+#         else:
+#             form = AuthenticationForm(request)
+#         context = {
+#             "form": form
+#         }
+#     return render(request, "health_records/index.html")
 
 
 def home_login_view(request):
@@ -12,11 +29,15 @@ def home_login_view(request):
             login(request, user)
             return redirect('user_profile')
         else:
-            form = AuthenticationForm(request)
-        context = {
-            "form": form
-        }
-    return render(request, "health_records/index.html")
+            messages.success(
+                request, ("Your username and/or password is incorrect. Please try again."))
+    else:
+        form = AuthenticationForm(request)
+    context = {
+        "form": form
+    }
+    return render(request, "health_records/index.html", context)
+    # return render(request, "health_records/index.html")
 
 
 # TODO: Error logic if incorrect user / password
