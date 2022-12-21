6b5aa33a50b9e731198e1bd8383c535bd14e07ba
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-12-21 09:09:31 -0600

    code cleanup

--- nwaukwaPHR/views.py
+++ nwaukwaPHR/views.py
@@ -4,22 +4,6 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 
 
-# def home_login_view(request):
-#     """Renders hompage and user login form"""
-#     if request.method == "POST":
-#         form = AuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             user = form.get_user()
-#             login(request, user)
-#             return redirect('user_profile')
-#         else:
-#             form = AuthenticationForm(request)
-#         context = {
-#             "form": form
-#         }
-#     return render(request, "health_records/index.html")
-
-
 def home_login_view(request):
     """Renders hompage and user login form"""
     if request.method == "POST":
@@ -37,7 +21,6 @@ def home_login_view(request):
         "form": form
     }
     return render(request, "health_records/index.html", context)
-    # return render(request, "health_records/index.html")
 
 
 # TODO: Error logic if incorrect user / password
